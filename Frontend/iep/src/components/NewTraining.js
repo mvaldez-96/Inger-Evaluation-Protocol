da651d8de6dfd34c3fbef3a777bf588cad718015
Maria Valdez <conivaldez438@gmail.com>
2020-06-17 16:28:56 -0700

    Router

---
+++ Frontend/iep/src/components/NewTraining.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewTraining extends Component {
+    render() {
+        return (
+            <div>
+                START NEW TRAINING
+            </div>
+        )
+    }
+}
